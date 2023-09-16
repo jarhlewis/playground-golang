4f882609f12cd5cd8ccb3904daeca5a2bb2c33b4
Jarh Lewis <jarhra96@gmail.com>
2023-09-16 23:05:38 +0200

    Add call to example reverse module

--- hello/hello.go
+++ hello/hello.go
@@ -2,21 +2,27 @@ package main
 
 import (
   "fmt"
-  "log"
+  // "log"
 
-  "play/greetings"
+  // "play/greetings"
+
+  "golang.org/x/example/hello/reverse"
 )
 
-func main() {
-  log.SetPrefix("greetings: ")
-  log.SetFlags(0) // don't print time, source file, and line number
+// func main() {
+//   log.SetPrefix("greetings: ")
+//   log.SetFlags(0) // don't print time, source file, and line number
 
-  names := []string{"Jarh", "Allan", "Ashley"}
+//   names := []string{"Jarh", "Allan", "Ashley"}
+
+//   message, err := greetings.Hellos(names)
+//   if err != nil {
+//     log.Fatal(err)
+//   }
   
-  message, err := greetings.Hellos(names)
-  if err != nil {
-    log.Fatal(err)
-  }
-  
-  fmt.Println(message)
+//   fmt.Println(message)
+// }
+
+func main() {
+  fmt.Println(reverse.String("Hello"), reverse.Int(24601))
 }
