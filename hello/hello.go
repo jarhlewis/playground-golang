57ce8774631f227201222258ff032e5eff82c644
Jarh Lewis <jarhra96@gmail.com>
2023-09-16 23:11:59 +0200

    Uncomment initial example code

--- hello/hello.go
+++ hello/hello.go
@@ -2,27 +2,24 @@ package main
 
 import (
   "fmt"
-  // "log"
+  "log"
 
-  // "play/greetings"
+  "play/greetings"
 
   "golang.org/x/example/hello/reverse"
 )
 
-// func main() {
-//   log.SetPrefix("greetings: ")
-//   log.SetFlags(0) // don't print time, source file, and line number
-
-//   names := []string{"Jarh", "Allan", "Ashley"}
-
-//   message, err := greetings.Hellos(names)
-//   if err != nil {
-//     log.Fatal(err)
-//   }
-  
-//   fmt.Println(message)
-// }
-
 func main() {
-  fmt.Println(reverse.String("Hello"))
+  log.SetPrefix("greetings: ")
+  log.SetFlags(0) // don't print time, source file, and line number
+
+  names := []string{"Jarh", "Allan", "Ashley"}
+
+  message, err := greetings.Hellos(names)
+  if err != nil {
+    log.Fatal(err)
+  }
+  
+  fmt.Println(message)
+  fmt.Println(reverse.String(message["Jarh"]))
 }
